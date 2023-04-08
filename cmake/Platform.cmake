c9005f120fafa7e67cd240de91e54010be8e198c
Mihail Mojsoski <mihail@mojsoski.mk>
2023-04-08 05:29:33 +0200

    Added better platform handling

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -1,6 +1,5 @@
 cmake_minimum_required(VERSION 3.0)
 
-# OS-specific stuff.
 if (UNIX AND NOT (APPLE OR HAIKU))
     add_compile_definitions(UNIX_LIKE)
     set(UNIX_LIKE 1)
@@ -24,12 +23,10 @@ if (HAIKU)
     add_compile_definitions(_BSD_SOURCE)
 endif (HAIKU)
 
-# If you build using "Debug" enable JCC_DEBUG.
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     add_compile_definitions(JCC_DEBUG)
 endif (CMAKE_BUILD_TYPE STREQUAL "Debug")
 
-# Set the common C compiler flags.
 set(CMAKE_C_STANDARD 23)
 set(CMAKE_C_FLAGS "-Wall -Wextra -Werror -fPIC")
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
@@ -42,16 +39,24 @@ else()
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0 -ggdb3")
 endif()
 
-# For the relative filename macro.
 string(LENGTH "${CMAKE_SOURCE_DIR}/" SOURCE_PATH_SIZE)
 add_compile_definitions("SOURCE_PATH_SIZE=${SOURCE_PATH_SIZE}")
 
-# Set the output to "bin".
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
-# Set the runtime path to "lib".
 IF (UNIX)
     SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
 ENDIF ()
+
+SET(JCC_PLATFORM "libc" CACHE STRING "The JCC platform")
+set(JCORE_LIB "jcore_${JCC_PLATFORM}")
+
+message("-- JCC platform: " ${JCC_PLATFORM})
+message("-- JCC core lib: " ${JCORE_LIB})
+
+add_library(${JCORE_LIB} SHARED src/core/string.c src/core/error.c src/core/logs.c)
+target_compile_definitions(${JCORE_LIB} PRIVATE JCC_IMPL)
+target_compile_definitions(${JCORE_LIB} PUBLIC "JCC_PLATFORM=${JCC_PLATFORM}")
+
--- lib/jcc/platform.h
+++ lib/jcc/platform.h
@@ -1,12 +1,14 @@
-#pragma once
+#ifndef JCC_INTERFACE
 
 #ifndef JCC_IMPL
-#error Cannot include <jcc/platform.h> outside of non-jcc executables.
+#error Cannot include <jcc/platform.h> outside of libcore.
 #endif
 
 #define JCC_PLATFORM_DEFAULT libc
 #ifndef JCC_PLATFORM
 #define JCC_PLATFORM JCC_PLATFORM_DEFAULT
+#define JCC_PLATFORM_DEFAULTED
 #endif
 
 #define JCC_INTERFACE(__interface__) <jcc/platform/JCC_PLATFORM/__interface__.interface.h>
+#endif
