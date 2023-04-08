75dbd75f191848274d91eddf3543f592664a5117
Mihail Mojsoski <mihail@mojsoski.mk>
2023-04-08 11:19:51 +0200

    len

--- cmake/Git.cmake
+++ cmake/Git.cmake
@@ -27,9 +27,7 @@ execute_process(COMMAND
         OUTPUT_VARIABLE
         GIT_CHANGES)
 
-message(RESULT = ${GIT_CHANGES_RESULT})
-message(CHANGES = ${GIT_CHANGES})
-string(LENGTH ${GIT_CHANGES} GIT_CHANGES_LEN)
+string(LENGTH "${GIT_CHANGES}" GIT_CHANGES_LEN)
 message(LENGTH = ${GIT_CHANGES_LEN})
 if(${GIT_CHANGES_LEN} EQUAL 0)
         add_compile_definitions(GIT_CLEAN)
